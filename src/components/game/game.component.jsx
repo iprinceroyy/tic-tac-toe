8662052c447b477b96d2a4ddb201934ac919ac38
Prince Roy <princeku.ray143@gmail.com>
2023-01-04 01:49:19 +0530

    complete game controls section

--- src/components/game/game.component.jsx
+++ src/components/game/game.component.jsx
@@ -1,15 +1,26 @@
 import Board from '../board/board.component';
+import './game.styles.scss';
+import logo from '../../assets/logo.svg';
+import iconX from '../../assets/icon-x-silver.svg';
+import restart from '../../assets/icon-restart.svg';
 
 const Game = () => {
 	const squares = Array(9).fill(null);
 
 	return (
-		<div>
-			<div className='game-controls'></div>
-
-			<div>
-				<Board squares={squares} />
+		<div className='game'>
+			<div className='game-controls'>
+				<img src={`${logo}`} alt=''></img>
+				<p>
+					<img src={`${iconX}`} alt=''></img>
+					<span>TURN</span>
+				</p>
+				<div>
+					<img src={`${restart}`} alt=''></img>
+				</div>
 			</div>
+
+			<Board squares={squares} />
 
 			<div className='scores'></div>
 		</div>
